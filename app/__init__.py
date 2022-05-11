cd640ba0a4a5e123aa4d4dbe1ccacd1cd5a37193
Natalya <chshebetun@gmail.com>
2022-05-11 09:51:05 -0700

    New model Human

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
-def create_app():
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(testing = None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    if testing == None:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    else:
+        app.config['TESTING'] = True    
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TEST_SQLALCHEMY_DATABASE_URI')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .models.cats import Cat
+    
+    from .routes.cats import cats_bp
+    app.register_blueprint(cats_bp)
 
     return app
---
+++ app/routes/cats.py
@@ -0,0 +1,130 @@
+from crypt import methods
+from flask import Blueprint, jsonify, request
+from app.models.cats import Cat
+from app import db
+from sqlalchemy_filters import apply_filters
+
+cats_bp = Blueprint('cats_bp', __name__, url_prefix='/cats')
+
+@cats_bp.route('', methods=['POST'])
+def create_one_cat():
+    request_body = request.get_json()
+    new_cat = Cat(name=request_body['name'],
+                age=request_body['age'],
+                color=request_body['color'])
+    db.session.add(new_cat)
+    db.session.commit()
+
+    return {
+        'id': new_cat.id,
+        'msg': f'Successfully created a cat with id {new_cat.id}'
+    }, 201
+
+@cats_bp.route('', methods=['GET'])
+def get_all_cats():
+    params = request.args
+    if 'color' in params and 'age' in params:
+        color_name = params['color']
+        age_value = params['age']
+        cats = Cat.query.filter_by(color=color_name, age=age_value)
+
+    elif 'color' in params:
+        color_name = params['color']
+        cats = Cat.query.filter_by(color=color_name)
+    elif 'age' in params:
+        age_value = params['age']
+        cats = Cat.query.filter_by(age=age_value)
+    else:
+        cats = Cat.query.all()
+    
+    # cats = Cat.query.all()
+    # params = request.args
+    # if params != None:
+    #     cats = apply_filters(cats, params)
+    # else:
+    #     cats = Cat.query.all()
+
+    cat_response = []
+    for cat in cats:
+        cat_response.append({
+            "id": cat.id,
+            "name": cat.name,
+            "age": cat.age,
+            "color": cat.color
+            })
+    return jsonify(cat_response), 200
+
+@cats_bp.route('/<cat_id>', methods=['GET'])
+def get_one_cat(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except ValueError:
+        rsp = {"msg": f"Invalid id: {cat_id}"}
+        return jsonify(rsp), 400
+
+    chosen_cat = Cat.query.get(cat_id)
+
+    if chosen_cat is None:
+        rsp = {"msg": f"Could not find cat with id {cat_id}"}
+        return jsonify(rsp), 404
+
+    rsp = {
+        "id": chosen_cat.id,
+        "name": chosen_cat.name,
+        "age": chosen_cat.age,
+        "color": chosen_cat.color
+    }
+    return jsonify(rsp), 200
+
+@cats_bp.route('/<cat_id>', methods=['PUT'])
+def put_one_can(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except ValueError:
+        rsp = {"msg": f"Invalid id: {cat_id}"}
+        return jsonify(rsp), 400
+    
+    chosen_cat = Cat.query.get(cat_id)
+
+    if chosen_cat is None:
+        rsp = {"msg": f"Could not find cat with id {cat_id}"}
+        return jsonify(rsp), 404
+    
+    request_body = request.get_json()
+
+    try:
+        chosen_cat.name = request_body['name']
+        chosen_cat.age = request_body['age']
+        chosen_cat.color = request_body['color']
+    except KeyError:
+        return {
+            "msg": "name, age, color are required"
+        }, 400
+    db.session.commit()
+
+    return f'Cat with id {chosen_cat.id} was successfully replaced', 200
+
+@cats_bp.route('/<cat_id>', methods=['DELETE'])
+def delete_cat(cat_id):
+    try:
+        cat_id = int(cat_id)
+    except ValueError:
+        rsp = {"msg": f"Invalid id: {cat_id}"}
+        return jsonify(rsp), 400
+    
+    chosen_cat = Cat.query.get(cat_id)
+
+    if chosen_cat is None:
+        rsp = {"msg": f"Could not find cat with id {cat_id}"}
+        return jsonify(rsp), 404
+    db.session.delete(chosen_cat)
+    db.session.commit()
+
+    return {
+        "msg": f"Cat with id {cat_id} was successfully destroyed"
+    }, 200
+
+
+#pgrep -fla flask   (give you a list of running servers)
+#kill -9 some_id_you_want_close (will be in the list)
+
